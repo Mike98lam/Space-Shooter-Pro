0ddd50eb6394b09840507f41c68f6ecc2229bbe2
Michael Lam <mike98lam@gmail.com>
2023-09-27 17:55:46 -0400

    Uploaded to itch.io

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class Enemy : MonoBehaviour
 {
--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.LowLevel;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class Powerup : MonoBehaviour
 {
